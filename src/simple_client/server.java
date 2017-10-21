e265ade1ad6b1683888fc0bc529ea351863c638d
Cataran <zradalol@gmail.com>
2017-10-21 12:24:10 +0300

    test

---
+++ src/simple_client/server.java
@@ -0,0 +1,7 @@
+package simple_client;
+
+/**
+ * Created by wtfor on 21.10.2017.
+ */
+public class server {
+}
---
+++ src/simple_server/client.java
@@ -0,0 +1,7 @@
+package simple_server;
+
+/**
+ * Created by wtfor on 21.10.2017.
+ */
+public class client {
+}
